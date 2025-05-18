098d0ca7438ab73a079f53ccb56f78705e37651d
Cecilia urrutia <urrutia.cici@gmail.com>
2025-05-18 17:06:58 -0300

    inicializo repositorio con creacion de database, funcion, vista y procedure

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# sistema-facturacion
-Sistema de facturacion, vistas, funciones, procedures.
+# Sistema de Facturación - MySQL
+
+### Descripción:
+Proyecto completo que simula un sistema de facturación en MySQL para prácticas de:
+- Procedimientos almacenados
+- Funciones y vistas
+
+### Tecnologías:
+- MySQL 8.x
+-MySQL Workbench
+
+### Estructura:
+1. Creación de base de datos
+2. Tablas y datos de ejemplo
+3. Funciones y procedimientos
+4. Vistas de reportes
+
+
+### Cómo probar:
+1. Ejecutar `sistema-facturacion.sql`
+3. Ver la vista `VistaFacturasConIVA`
+4. Llamar al procedimiento:
+CALL ReporteFacturacionMensual();
---
+++ sistema-facturacion.sql
@@ -0,0 +1,108 @@
+
+-- DROP DATABASE IF EXISTS FacturacionDB; 
+
+CREATE DATABASE IF NOT EXISTS FacturacionDB;
+USE FacturacionDB;
+
+
+-- Tabla clientes
+CREATE TABLE Clientes (
+    ClienteID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    Email VARCHAR(100),
+    Direccion VARCHAR(100),
+    Ciudad VARCHAR(50)
+);
+
+-- Tabla facturas
+CREATE TABLE Facturas (
+    FacturaID INT AUTO_INCREMENT PRIMARY KEY,
+    ClienteID INT,
+    Fecha DATE,
+    Monto DECIMAL(10, 2),
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
+);
+
+-- Insertar clientes
+INSERT INTO Clientes (Nombre, Apellido, Email, Direccion, Ciudad) VALUES
+('Juan',  'Pérez', 'juan@example.com', 'Quito 19', 'Madrid'),
+('Ana', ' Torres', 'ana@example.com', 'Canillejas 12', 'Madrid'),
+('Pedro', 'Jose', 'pedro@example.com', 'Alabama 26', 'Toledo')
+;
+
+-- Insertar facturas
+
+INSERT INTO Facturas (ClienteID, Fecha, Monto) VALUES
+(1, '2025-05-01', 1000),
+(1, '2025-05-15', 2300),
+(2, '2025-05-20', 1500);
+
+-- Calculo de IVA
+-- Función para calcular el IVA (21%)
+DELIMITER $$
+
+CREATE FUNCTION CalcularIVA (monto DECIMAL(10,2))
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    RETURN monto * 0.21;
+END$$
+
+DELIMITER ;
+
+-- Procedure para reporte mensual
+
+DROP PROCEDURE ReporteFacturacionMensual;
+
+DELIMITER $$
+CREATE PROCEDURE ReporteFacturacionMensual()
+BEGIN
+    SELECT 
+        c.Nombre, c.Apellido, c.Email,
+        DATE_FORMAT(f.Fecha, '%Y-%m') AS Mes,
+        SUM(f.Monto) AS TotalFacturado
+    FROM Facturas f
+    JOIN Clientes c ON f.ClienteID = c.ClienteID
+    GROUP BY c.Nombre, DATE_FORMAT(f.Fecha, '%Y-%m');
+END$$
+
+DELIMITER ;
+
+-- Vista de reposrtes
+
+CREATE OR REPLACE VIEW VistaFacturasConIVA AS
+SELECT 
+    f.FacturaID,
+    c.Nombre,
+    c.Apellido,
+    c.Email,
+    f.Fecha,
+    f.Monto,
+    CalcularIVA(f.Monto) AS IVA,
+    f.Monto + CalcularIVA(f.Monto) AS TotalConIVA
+FROM Facturas f
+JOIN Clientes c ON f.ClienteID = c.ClienteID;
+
+-- Ver la vista de facturacion
+
+select * FROM VistaFacturasConIva;
+
+SHOW CREATE VIEW Vista_Facturacion;
+
+-- Muestro la vista
+
+SHOW FULL TABLES IN FacturacionDB WHERE TABLE_TYPE = 'VIEW';
+
+-- Ver vista facturacion con iva
+
+SELECT * FROM VistaFacturasConIva;
+
+-- verifiar si el procedure esta creado
+
+SHOW PROCEDURE STATUS WHERE Db = 'FacturacionDB';
+
+
+-- ejecutar el procedure
+
+CALL ReporteFacturacionMensual();
